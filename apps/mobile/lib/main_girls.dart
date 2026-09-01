33031308b6e6b606461aba5bae82f53437192ac4
yokina <mail@cloxs.jp>
2026-09-01 18:35:16 +0900

    Add MinApp Girls entrypoint

---
+++ apps/mobile/lib/main_girls.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'endpoint_validation.dart';
+import 'girls_app.dart';
+import 'hosted_girls_api.dart';
+
+void main() {
+  const String rawHostedApiBaseUrl = String.fromEnvironment(
+    'MINAPP_HOSTED_API_BASE_URL',
+  );
+  if (rawHostedApiBaseUrl.isEmpty) {
+    throw StateError(
+      'MINAPP_HOSTED_API_BASE_URL is required for MinApp Girls. '
+      'Run with --dart-define=MINAPP_HOSTED_API_BASE_URL=https://...',
+    );
+  }
+
+  final Uri hostedApiBaseUri = validatePublicHttpsBaseUri(
+    Uri.parse(rawHostedApiBaseUrl),
+    argumentName: 'MINAPP_HOSTED_API_BASE_URL',
+  );
+
+  runApp(
+    GirlsApp(
+      api: HostedGirlsApi(baseUri: hostedApiBaseUri),
+    ),
+  );
+}
