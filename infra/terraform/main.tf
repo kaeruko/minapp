d5deb16e62a9b2d9aecb0554959db5b7fbc09b19
yokina <mail@cloxs.jp>
2026-09-06 17:01:38 +0900

    Increase API Lambda memory to 512 MB

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -247,7 +247,7 @@ resource "aws_lambda_function" "api" {
   filename         = data.archive_file.api.output_path
   source_code_hash = data.archive_file.api.output_base64sha256
 
-  memory_size = 128
+  memory_size = 512
   timeout     = 10
 
   environment {
