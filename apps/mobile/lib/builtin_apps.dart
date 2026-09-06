9d8245a72e0bd921e38ea026ac77879210448326
yokina <mail@cloxs.jp>
2026-09-07 07:34:35 +0900

    Remove local Novel Player from Flutter catalog

--- apps/mobile/lib/builtin_apps.dart
+++ apps/mobile/lib/builtin_apps.dart
@@ -82,18 +82,6 @@ const List<BuiltInApp> builtInApps = <BuiltInApp>[
     iconBorderColor: Color(0xFF818CF8),
     iconColor: Color(0xFF4338CA),
   ),
-  BuiltInApp(
-    id: 'novel-starter',
-    title: 'ひみつの放課後',
-    assetPath: 'assets/builtin/novel_starter/index.html',
-    searchableText:
-        'ひみつの放課後 ノベルゲーム 女子向け 恋愛 男子 白髪 イケメン レン 小説 ストーリー 選択肢 キャラクター 顔 イラスト AI 改造 スターター みんアプ公式 サンプル',
-    icon: Icons.auto_stories_rounded,
-    cardColor: Color(0xFFF5F3FF),
-    iconBackgroundColor: Color(0xFFE9D5FF),
-    iconBorderColor: Color(0xFFA78BFA),
-    iconColor: Color(0xFF6D28D9),
-  ),
 ];
 
 List<BuiltInApp> filterBuiltInApps(String query) {
